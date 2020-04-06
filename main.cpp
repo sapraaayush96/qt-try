00e6bff612d510811755cddf2cccac6a4e53f7e6
sapraaayush96 <31188566+sapraaayush96@users.noreply.github.com>
2020-04-06 17:24:14 +0530

    done

--- main.cpp
+++ main.cpp
@@ -1,8 +1,9 @@
 #include <QCoreApplication>
-
+#include<iostream>
+using namespace std;
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-
+cout<<"hello world";
     return a.exec();
 }
