84bfd3fd930365dbb8a7a82bd19379e3b2669aaa
sapraaayush96 <31188566+sapraaayush96@users.noreply.github.com>
2020-04-06 17:10:21 +0530

    Delete main.cpp

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <QCoreApplication>
-#include<iostream>
-using namespace std;
-int main(int argc, char *argv[])
-{
-    QCoreApplication a(argc, argv);
-cout<<"hello world";
-    return a.exec();
-}
